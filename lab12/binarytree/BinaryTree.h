6d86295b5b35c68f04155a399a70ef87158d3661
pboron <caorson@gmail.com>
2017-05-25 10:54:19 +0200

    Merge branch 'master' of https://gitlab.com/agh-jimp2/exercises
    # Conflicts:
    #	lab12/CMakeLists.txt

--- lab12/binarytree/BinaryTree.h
+++ lab12/binarytree/BinaryTree.h
@@ -5,4 +5,30 @@
 #ifndef JIMP_EXERCISES_BINARYTREE_H
 #define JIMP_EXERCISES_BINARYTREE_H
 
+#include <ostream>
+#include <string>
+#include <memory>
+
+
+    class SmartTree{
+    public:
+        int value;
+        std::unique_ptr<SmartTree> left,right;
+    };
+
+    std::unique_ptr <SmartTree> CreateLeaf(int value);
+    std::unique_ptr <SmartTree> InsertLeftChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> left_subtree);
+    std::unique_ptr <SmartTree> InsertRightChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> right_subtree);
+    void PrintTreeInOrder(const std::unique_ptr<SmartTree> &unique_ptr, std::ostream *out);
+    std::string DumpTree(const std::unique_ptr<SmartTree> &tree);
+    std::unique_ptr <SmartTree> RestoreTree(const std::string &tree);
+
+    class Leaf{
+
+    };
+
+    class BinaryTree{
+
+    };
+
 #endif //JIMP_EXERCISES_BINARYTREE_H
